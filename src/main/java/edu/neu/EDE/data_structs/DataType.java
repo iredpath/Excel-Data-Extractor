cbfcfbc36cdb4a982403146e869f610cb985322a
TStod <stoddard.t@husky.neu.edu>
2015-03-22 13:40:03 -0400

    More Javadoc
    Javadock for DataType and SheetConfiguration

--- src/main/java/edu/neu/EDE/data_structs/DataType.java
+++ src/main/java/edu/neu/EDE/data_structs/DataType.java
@@ -1,5 +1,9 @@
 package edu.neu.EDE.data_structs;
 
+/**
+ * @author Ian Redpath
+ * @version 3/22/2015
+ */
 public enum DataType {
     STATISTIC, STIMULUS, SUBJECT
 }
--- src/main/java/edu/neu/EDE/data_structs/SheetConfiguration.java
+++ src/main/java/edu/neu/EDE/data_structs/SheetConfiguration.java
@@ -1,20 +1,28 @@
 package edu.neu.EDE.data_structs;
 
+/**
+ * Used for setting and getting values from the four dimensional array
+ * @author Ian Redpath
+ * @version 3/22/2015
+ */
 public class SheetConfiguration {
 
     private String statistic;
-    private String media;
+    private String media; // only used for setting
     private String stimulus;
     private String subject;
     private Double value; // only used for setting
 
-    public SheetConfiguration() {}
-
-    /* 
-     * this set will only be used by the WorkbookWriter code.
-     * when we don't know which field we are setting 
+    /**
+     * Blank Constructor for SheetConfiguration
      */
+    public SheetConfiguration() {}
     
+    /**
+     * Used to set the sheet configuration
+     * @param type DataType Enum
+     * @param value String that we are using
+     */
     public void set(DataType type, String value) {
         if (type.equals(DataType.STATISTIC)) {
             this.statistic = value;
@@ -25,24 +33,83 @@ public class SheetConfiguration {
         }
     }
 
-    public String getStatistic() { return this.statistic; }
+    /**
+     * Used for getting the statistic stored in the SheetConfiguration
+     * @return String that is the statistic that is stored in the SheetConfiguration
+     */
+    public String getStatistic() {
+        return this.statistic;
+    }
 
-    public String getStimulus() { return this.stimulus; }
+    /**
+     * Used for getting the stimulus stored in the SheetConfiguration
+     * @return String that is the stimulus that is stored in the SheetConfiguration
+     */
+    public String getStimulus() {
+        return this.stimulus;
+    }
 
-    public String getMedia() { return this.media; }
+    /**
+     * Used for getting the media stored in the SheetConfiguration
+     * @return String that is the media that is stored in the SheetConfiguration
+     */
+    public String getMedia() {
+        return this.media;
+    }
 
-    public String getSubject() { return this.subject; }
+    /**
+     * Used for getting the subject stored in the SheetConfiguration
+     * @return String that is the subject that is stored in the SheetConfiguration
+     */
+    public String getSubject() {
+        return this.subject;
+    }
 
-    public Double getValue() { return this.value; }
+    /**
+     * Used for getting the value stored in the SheetConfiguration
+     * @return String that is the value that is stored in the SheetConfiguration
+     */
+    public Double getValue() {
+        return this.value;
+    }
 
-    public void setStatistic(String stat) { this.statistic = stat; }
+    /**
+     * Used for setting the statistic stored in the SheetConfiguration
+     * @param stat String that we are setting the statistic to in the SheetConfiguration
+     */
+    public void setStatistic(String stat) {
+        this.statistic = stat;
+    }
+    
+    /**
+     * Used for setting the stimulus stored in the SheetConfiguration
+     * @param stat String that we are setting the stimulus to in the SheetConfiguration
+     */
+    public void setStimulus(String stim) {
+        this.stimulus = stim;
+    }
 
-    public void setStimulus(String stim) { this.stimulus = stim; }
+    /**
+     * Used for setting the media stored in the SheetConfiguration
+     * @param stat String that we are setting the media to in the SheetConfiguration
+     */
+    public void setMedia(String med) {
+        this.media = med;
+    }
 
-    public void setMedia(String med) { this.media = med; }
+    /**
+     * Used for setting the subject stored in the SheetConfiguration
+     * @param stat String that we are setting the subject to in the SheetConfiguration
+     */
+    public void setSubject(String subj) {
+        this.subject = subj;
+    }
 
-    public void setSubject(String subj) { this.subject = subj; }
-
-    public void setValue(Double d) { this.value = d; }
-
+    /**
+     * Used for setting the value stored in the SheetConfiguration
+     * @param stat String that we are setting the value to in the SheetConfiguration
+     */
+    public void setValue(Double val) {
+        this.value = val;
+    }
 }
